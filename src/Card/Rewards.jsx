6bad6efceade11478cd71060f935a98fd4d15f0e
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-11 01:17:44 +0200

    refactor: modify the reward structure to make it more readable

--- src/Card/Rewards.jsx
+++ src/Card/Rewards.jsx
@@ -1,12 +1,51 @@
-function Rewards({ rewards, levelRequiment }) {
-  const reward = () => {
-    return alert(`Requires a minimum level of ${levelRequiment}`);
+function Rewards({ rewards, levelRequirement }) {
+  const getEmoji = (item) => {
+    if (item.isUnique) return "👑";
+    switch (item.type) {
+      case "sword":
+      case "dagger":
+      case "weapon":
+        return "⚔️";
+      case "armor":
+      case "helm":
+        return "🛡️";
+      case "scroll":
+        return "📜";
+      case "consumable":
+        return "🧪";
+      case "artifact":
+      case "amulet":
+      case "quest_item":
+        return "💎";
+      default:
+        return "🎁";
+    }
   };
+
+  ///// Event Onclick to show the name in a tooltip.
+
   return (
-    <div>
-      <button className="reward-container" onClick={reward}>
-        Reward of {rewards.experience} experience and {rewards.gold} gold
-      </button>
+    <div className="rewards-container">
+      <p>
+        <strong>Level Requirement:</strong> {levelRequirement}
+      </p>
+      <p>
+        <strong>Experience:</strong> {rewards.experience}
+      </p>
+      <p>
+        <strong>Gold:</strong> {rewards.gold}
+      </p>
+      <div className="rewards-line">
+        <span className="rewards-label">
+          <strong>Rewards:</strong>
+        </span>
+        <div className="reward-buttons">
+          {rewards.items &&
+            rewards.items.map((item) => (
+              <button className="reward-btn">{getEmoji(item)}</button>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
